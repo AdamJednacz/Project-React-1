c79f9650acea8bf8ecf1f7beb4755168bbc17f24
AdamJednacz <adamjednacz4@gmail.com>
2024-08-01 18:21:02 +0200

    contact card

--- my-app/src/components/Contact/Contact_Main.js
+++ my-app/src/components/Contact/Contact_Main.js
@@ -12,8 +12,8 @@ const ContactMain = () => {
                     <div className="card info">
                         <img src={logo} alt="logo"/>
                         <p><span>Sebastian Kowalik</span></p>
-                        <p><span>Telefon:</span> <a href="tel:+48793316004" className="contact_number">+48 793 316 004</a></p>
-                        <p><span>E-mail:</span> sebastiankowalik_@op.pl</p>
+                        <p><a href="tel:+48793316004" className="contact_number"><span>+48 793 316 004</span></a></p>
+                        <p><span>sebastiankowalik_@op.pl</span></p>
                       <a  href="https://www.facebook.com/p/Kowalik-Kamieniarstwo-100063562874398/?paipv=0&eav=AfaS-ieRF-mB41uRlunv-JZtScZZ2CHmjlEUoJwFdVR35KEyDq4sa0UdIuyDL6wovTg&_rdr" className=' facebook icon'><FontAwesomeIcon className="hoveredBlue fb" icon={faFacebook} />  Facebook</a>
 
                     </div>
