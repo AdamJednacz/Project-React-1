7b20ff00e1799752d31d2d835fe81b2515213b0f
AdamJednacz <adamjednacz4@gmail.com>
2024-08-01 17:21:40 +0200

    test s sliderem

--- my-app/src/components/Home/Home_Main.js
+++ my-app/src/components/Home/Home_Main.js
@@ -1,27 +1,40 @@
-import React, {lazy} from 'react';
-
+import React, {lazy, useEffect, useState} from 'react';
 import {Link} from "react-router-dom";
 import {style} from "../../App";
 
-const Home_Slider  = lazy(() => import("../Home/Home_Slider"));
+const Home_Slider = lazy(() => import("../Home/Home_Slider"));
+
 const Home_Main = () => {
+    const [isWideScreen, setIsWideScreen] = useState(window.matchMedia("(min-width: 1381px)").matches);
+
+    useEffect(() => {
+        const mediaQuery = window.matchMedia("(min-width: 1381px)");
+
+        const handleMediaQueryChange = (e) => {
+            setIsWideScreen(e.matches);
+        };
+
+        mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+        return () => {
+            mediaQuery.removeEventListener('change', handleMediaQueryChange);
+        };
+    }, []);
+
     return (
         <div className='bg' style={style}>
-        <main className="home_main">
-            <div className="container" >
-                <div className="main_card">
-                    <h1>Lorem Ipsum</h1>
-                    <h2>Lorem Ipsum Dolor</h2>
-                    <span>Ipsum</span>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    <Link className='Link' to='/onas'><button className='btn_main'>Zobacz więcej</button></Link>
+            <main className="home_main">
+                <div className="container" >
+                    <div className="main_card">
+                        <h1>Lorem Ipsum</h1>
+                        <h2>Lorem Ipsum Dolor</h2>
+                        <span>Ipsum</span>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <Link className='Link' to='/onas'><button className='btn_main'>Zobacz więcej</button></Link>
+                    </div>
+                    {isWideScreen && <Home_Slider />}
                 </div>
-
-                    <Home_Slider />
-
-            </div>
-
-        </main>
+            </main>
         </div>
     );
 };
